8cc16887874aad9bdadcf6c284fd335d033ff728
jascodebrewer <jasofficial1214@gmail.com>
2023-12-11 19:03:31 +0530

    updated code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='jas meet',
+    author_email='jasofficial1214@gmail.com',
+    packages=find_packages()
+)
